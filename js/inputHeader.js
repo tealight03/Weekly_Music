a6973eb7c4cab13419fcfa6d02d1ffd985f2e8a9
min-young417 <nonamed1021@naver.com>
2023-08-13 22:14:47 +0900

    오류 수정

--- js/inputHeader.js
+++ js/inputHeader.js
@@ -1,7 +1,37 @@
 var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
-        document.getElementById("header_container").innerHTML = this.responseText;
+        const headerContainer = document.getElementById("header_container");
+        headerContainer.innerHTML = this.responseText;
+
+        // 스크립트 태그 동적으로 생성하여 추가
+        const scripts = headerContainer.getElementsByTagName('script');
+        for (let i = 0; i < scripts.length; i++) {
+            const newScript = document.createElement('script');
+            newScript.textContent = scripts[i].textContent;
+            headerContainer.appendChild(newScript);
+        }
+
+            const state = document.querySelector('.state');
+            if(document.cookie.indexOf('user=') === -1) {
+                const loginBtn = document.createElement('button');
+                loginBtn.textContent = '로그인';
+                loginBtn.id = 'login_btn';
+                loginBtn.addEventListener('click', e => {
+                    location.href = '/login';
+                });
+                state.append(loginBtn);
+            }
+            else {
+                const logoutBtn = document.createElement('button');
+                logoutBtn.textContent = '로그아웃';
+                logoutBtn.id = 'login_btn';
+                logoutBtn.addEventListener('click', e => {
+                    location.href = '/logout';
+                });
+                state.append(logoutBtn);
+            }
+
     }
 };
 xhttp.open("GET", "/menu", true);
